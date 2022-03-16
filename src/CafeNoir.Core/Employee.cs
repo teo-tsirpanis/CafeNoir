a535a7ef289460772e1b81fcdaf9595c41a0dc93
Giannis Polychroniadis <77056849+greeneight-p@users.noreply.github.com>
2022-03-16 15:33:26 +0200

    Base Draft (#5)
    Co-authored-by: Polyc <Polyc@JOHN-DESKTOP>

---
+++ src/CafeNoir.Core/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeNoir.Core {
+
+
+    public enum EmployeeType {
+        Magager,
+        Cashier,
+        Barista,
+        Waiter
+    }
+    public class Employee {
+
+
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }    
+        public string Surname { get; set; }
+
+        public EmployeeType EmployeeType { get; set; }
+
+        public int SallaryPerMonth { get; set; }
+
+        public Employee() {
+
+        }
+    }
+}
