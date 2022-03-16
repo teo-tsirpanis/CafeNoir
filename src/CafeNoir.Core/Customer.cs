fa204522e53ddcbdf6b90d48ff397fe21c0ed842
Polyc <Polyc@JOHN-DESKTOP>
2022-03-16 15:24:38 +0200

    Base Draft

---
+++ src/CafeNoir.Core/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeNoir.Core;
+
+public class Customer {
+    public Guid ID { get; } = Guid.NewGuid();
+    public string Code { get; set; }
+    public string Description { get; set; } = string.Empty;
+
+    public static Customer RetailCustomer { get; } = new Customer("001")
+    {
+        Description = "Retail Customer"
+    };
+
+    public Customer(string code) {
+        Code=code;
+    }
+
+
+
+}
---
+++ src/CafeNoir.Core/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeNoir.Core {
+    public class Product {
+
+        public Guid ID { get; set; }
+        public string Code { get; set; }
+
+
+        public Product() {
+
+        }
+    }
+}
