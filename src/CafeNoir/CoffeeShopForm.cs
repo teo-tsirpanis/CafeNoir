b32a963e5d2e2a639eafcb55a45a2334ecfedfbb
Polyc <Polyc@JOHN-DESKTOP>
2022-03-17 14:58:08 +0200

    fix load error, customer counter, coffee shop status messagebox

--- src/CafeNoir/CoffeeShopForm.cs
+++ src/CafeNoir/CoffeeShopForm.cs
@@ -18,6 +18,10 @@ public partial class CoffeeShopForm : Form
     private void CoffeeShopForm_Load(object sender, EventArgs e) {
         
         saveToolStripMenuItem.Enabled = false;
+        newDayToolStripMenuItem.Enabled = false;
+        todaysCustomerToolStripMenuItem.Enabled = false;
+        coffeeShopStatusToolStripMenuItem.Enabled=false;
+        
 
 
         //CoffeeShop cf = new CoffeeShop();
@@ -37,15 +41,9 @@ public partial class CoffeeShopForm : Form
 
 
 
-
-
     private void loadToolStripMenuItem_Click(object sender, EventArgs e) {
         LoadData();
-        MessageBox.Show(this, $"Managers: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Magager)}, " +
-            $"Cashiers: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Cashier)}, " +
-            $"Baristas: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Barista)}, " +
-            $"Waiters: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Waiter)}, " + 
-            $"Customers: {_coffeeshop.Customers.Count}");
+        
 
     }
 
@@ -55,26 +53,68 @@ public partial class CoffeeShopForm : Form
     }
 
 
+    private void listToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        LoadData();
+        CustomerForm customerForm = new CustomerForm();
+        customerForm.CoffeeShop = _coffeeshop;
+        customerForm.ShowDialog();
+    }
+
+    private void dayCustomerCountToolStripMenuItem_Click(object sender, EventArgs e) {
+        MessageBox.Show($"Today's Customers: {_coffeeshop.CustomersToday}");
+    }
+
+    private void newDayToolStripMenuItem_Click(object sender, EventArgs e) {
+        _coffeeshop.CustomersToday = 0;
+        if(_coffeeshop.Customers.Count == 1) {
+            _coffeeshop.Customers.Remove(_coffeeshop.Customers.Last());
+        }
+
+        SaveData();
+        MessageBox.Show("New day.!!");
+    }
+
+
+    private void CoffeeShopInitialize() {
+        CoffeeShop cf = new CoffeeShop();
+        _coffeeshop = cf;
+    }
+
 
     public void LoadData() {
 
-        string json = File.ReadAllText(FILE_NAME);
-        _coffeeshop = JsonSerializer.Deserialize<CoffeeShop>(json);
+        try {
+           string json = File.ReadAllText(FILE_NAME);
+           _coffeeshop = JsonSerializer.Deserialize<CoffeeShop>(json);
+            MessageBox.Show("Load Successfully");
+        }
+        catch (Exception) {
+            MessageBox.Show("File Not Found, New File Created");
+            CoffeeShopInitialize();
+            //throw;
+        }
+
         saveToolStripMenuItem.Enabled = true;
+        coffeeShopStatusToolStripMenuItem.Enabled = true;
+        newDayToolStripMenuItem.Enabled = true;
+        todaysCustomerToolStripMenuItem.Enabled = true;
+
 
 
     }
-
 
     public void SaveData() {
         string json = JsonSerializer.Serialize(_coffeeshop);
         File.WriteAllText(FILE_NAME, json);
     }
 
-    private void listToolStripMenuItem_Click(object sender, EventArgs e) {
-        LoadData();
-        CustomerForm customerForm = new CustomerForm();
-        customerForm.CoffeeShop = _coffeeshop;
-        customerForm.ShowDialog();
+    private void coffeeShopStatusToolStripMenuItem_Click(object sender, EventArgs e) {
+        MessageBox.Show(this, $"Managers: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Magager)}, " +
+            $"Cashiers: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Cashier)}, " +
+            $"Baristas: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Barista)}, " +
+            $"Waiters: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Waiter)}, " +
+            $"Active Customer: {_coffeeshop.Customers.Count} " +
+            $"Customers today: {_coffeeshop.CustomersToday}");
     }
 }
