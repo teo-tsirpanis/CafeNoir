57f7ed5fafb3648979031c1c1ee2868ca5a24ea4
Jimbos <tr9l9l9@hotmail.com>
2022-03-17 14:07:29 +0200

    Refactored and cleaned up CoffeeShopForm

--- src/CafeNoir/CoffeeShopForm.cs
+++ src/CafeNoir/CoffeeShopForm.cs
@@ -8,33 +8,39 @@ public partial class CoffeeShopForm : Form {
     private const string FILE_NAME = "coffeeshop.json";
     private CoffeeShop _coffeeshop;
 
-
     public CoffeeShopForm() {
         InitializeComponent();
     }
-
     private void CoffeeShopForm_Load(object sender, EventArgs e) {
         LoadData();
         saveToolStripMenuItem.Enabled = false;
     }
-
+    #region UI
     private void loadToolStripMenuItem_Click(object sender, EventArgs e) {
         LoadData();
+        DisplayCoffeeShopState();
+    }
+    private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+        SaveData();
+        MessageBox.Show("Saved");
+    }
+    private void listToolStripMenuItem_Click(object sender, EventArgs e) {
+        LoadData();
+        CustomerForm customerForm = new CustomerForm();
+        customerForm.CoffeeShop = _coffeeshop;
+        customerForm.ShowDialog();
+    }
+    #endregion
+
+    #region Methods
+    private void DisplayCoffeeShopState() {
         MessageBox.Show(this, $"Managers: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Magager)}, " +
             $"Cashiers: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Cashier)}, " +
             $"Baristas: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Barista)}, " +
             $"Waiters: {_coffeeshop.Employess.Count(x => x.EmployeeType == EmployeeType.Waiter)}, " +
             $"Customers: {_coffeeshop.Customers.Count}");
-
     }
-
-    private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
-        SaveData();
-        MessageBox.Show("Saved");
-    }
-
     public void LoadData() {
-
         if (!File.Exists(FILE_NAME)) {
             using (File.Create(FILE_NAME)) ;
         }
@@ -48,19 +54,10 @@ public partial class CoffeeShopForm : Form {
             _coffeeshop = new CoffeeShop();
             SaveData();
         }
-
     }
-
-
     public void SaveData() {
         string json = JsonSerializer.Serialize(_coffeeshop);
         File.WriteAllText(FILE_NAME, json);
     }
-
-    private void listToolStripMenuItem_Click(object sender, EventArgs e) {
-        LoadData();
-        CustomerForm customerForm = new CustomerForm();
-        customerForm.CoffeeShop = _coffeeshop;
-        customerForm.ShowDialog();
-    }
+    #endregion
 }
