4eba1820019af2c638d8dd4fd680f1526460af11
Jimbos <tr9l9l9@hotmail.com>
2022-03-17 12:52:11 +0200

    Created first draft of the TransactionLine Class

---
+++ src/CafeNoir.Core/TransactionLine.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CafeNoir.Core;
+public class TransactionLine {
+    public Guid ID { get; set; }
+    public Guid ProductID { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal DiscountPerCent { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    public TransactionLine() {
+        ID = Guid.NewGuid();
+    }
+    public TransactionLine(Guid productID) : this() {
+        ProductID = productID;
+    }
+    public TransactionLine(Guid productID, int quantity, decimal price, decimal discountPerCent) : this() {
+        ProductID = productID;
+        Quantity = quantity;
+        Price = price;
+        DiscountPerCent = discountPerCent;
+        TotalPrice = Quantity * Price;
+    }
+    public TransactionLine(Guid productID, int quantity, decimal price, decimal discountPerCent, decimal totalPrice) : this() {
+        ProductID = productID;
+        Quantity = quantity;
+        Price = price;
+        DiscountPerCent = discountPerCent;
+        TotalPrice = totalPrice;
+    }
+}
