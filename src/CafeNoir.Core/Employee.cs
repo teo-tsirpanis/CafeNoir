1f40943510f6c89817290b39f53ea063f93a8932
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-03-18 10:29:07 +0200

    Fix `Employee.SallaryPerMonth`'s typo and make it a decimal.

--- src/CafeNoir.Core/Employee.cs
+++ src/CafeNoir.Core/Employee.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CafeNoir.Core {
-
-    public class Employee {
-
-
+﻿namespace CafeNoir.Core
+{
+    public class Employee
+    {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string Name { get; set; }    
+        public string Name { get; set; }
+
         public string Surname { get; set; }
 
         public EmployeeType EmployeeType { get; set; }
 
-        public int SallaryPerMonth { get; set; }
+        public decimal SalaryPerMonth { get; set; }
 
-        public Employee() {
+        public Employee()
+        {
 
         }
     }
