56a2495c4be9a96c2c4771c8f7aa7d1c8aecd685
Jimbos <tr9l9l9@hotmail.com>
2022-03-20 18:05:41 +0200

    Refactored Code on Monthly ledger. Added a renting cost field @Coffee Shop class

--- src/CafeNoir/MonthlyLedgerForm.cs
+++ src/CafeNoir/MonthlyLedgerForm.cs
@@ -1,67 +1,70 @@
 ﻿using CafeNoir.Core;
-using DevExpress.XtraEditors.Controls;
-using DevExpress.XtraEditors.Repository;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace CafeNoir {
     public partial class MonthlyLedgerForm : Form {
+        private decimal _mExpenses = 0m;
+        private decimal _mIncome = 0m;
+        private decimal _mTotal = 0m;
+        private decimal _mFixedExpenses = 0m;
 
         public CoffeeShop CoffeeShop { get; set; }
         public MonthlyLedgerForm(CoffeeShop _coffeeShop) {
             CoffeeShop = _coffeeShop;
             InitializeComponent();
         }
+        private void MonthlyLedgerForm_Load(object sender, EventArgs e) {
+
+            ControlExtensions.PopulateLedgerMonths(lookUpEditMonths.Properties);
+            UpdateFields();
+        }
+        #region UI
         private void btnClose_Click(object sender, EventArgs e) {
             this.Close();
         }
         private void simpleButtonShow_Click(object sender, EventArgs e) {
+            UpdateMonthlyLedger();
+        }
+        #endregion
+
+        #region Methods
+        private void UpdateMonthlyLedger() {
+            SetUpTransactionGrid();
+
+            grvTransactions.RefreshData();
+
+            UpdateFields();
+        }
+        private void SetUpTransactionGrid() {
             int year = Convert.ToInt32(spinEditYear.EditValue);
             int month = Convert.ToInt32(lookUpEditMonths.EditValue);
 
-
-            decimal expenses = 0;
-            decimal income = 0;
-            decimal profit = 0; 
-
-
+            var transactionsConfirmed = GetLedgerOfSelectedMonth(year, month);
+            bsTransactions.DataSource = transactionsConfirmed;
+            grdTransactions.DataSource = bsTransactions;
+        }
+        private void UpdateFields() {
+            _mFixedExpenses = CoffeeShop.RentingCost;
+            _mTotal = _mIncome - _mExpenses - _mFixedExpenses;
+            labelIncome.Text = "Income: +" + _mIncome.ToString() + " €";
+            labelExpenses.Text = "Expenses: -" + _mExpenses.ToString() + " €";
+            labelFixedExpenses.Text = "Fixed expenses: -" + _mFixedExpenses + " €";
+            labelTotal.Text = "Profit: = " + _mTotal.ToString() + " €";
+        }
+        private List<Transaction> GetLedgerOfSelectedMonth(int year, int month) {
             var list = CoffeeShop.Transactions;
             List<Transaction> transactionsConfirmed = new List<Transaction>();
 
 
             foreach (Transaction item in list) {
-                if(item.Date.Year == year && item.Date.Month == month) {
-                    income += item.TotalPrice;
-                    expenses += item.TotalCost;
+                if (item.Date.Year == year && item.Date.Month == month) {
+                    _mIncome += item.TotalPrice;
+                    _mExpenses += item.TotalCost;
                     transactionsConfirmed.Add(item);
 
                 }
             }
-            bsTransactions.DataSource = transactionsConfirmed;
-            grdTransactions.DataSource = bsTransactions;
-            grvTransactions.RefreshData();
-
-
-            profit = income - expenses - 3000;
-
-            labelExpenses.Text = "Expenses: "+expenses.ToString()+ " €";
-            labelIncome.Text = "Income: "+income.ToString() + " €";
-            labelFixedExpenses.Text = "Profit: "+profit.ToString() + " €";
-
+            return list;
         }
-
-
-
-        private void MonthlyLedgerForm_Load(object sender, EventArgs e) {
-
-           ControlExtensions.PopulateLedgerMonths(lookUpEditMonths.Properties);
-        }
+        #endregion
     }
 }
